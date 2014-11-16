aace3f13c409a424f019d157c176e4d24acd5bc5
James <james@jarofgreen.co.uk>
2014-11-16 15:21:36 +0000

    Don't run on blank pages

--- chrome/button.js
+++ chrome/button.js
@@ -1,8 +1,10 @@
 	CustomButton = {
 
 1: function () {
-	var win = window.open("https://www.blocked.org.uk/results?url=" + encodeURIComponent(gBrowser.contentDocument.location), '_blank');
- 	win.focus();
+	if ("about:startpage" != gBrowser.contentDocument.location) {
+		var win = window.open("https://www.blocked.org.uk/results?url=" + encodeURIComponent(gBrowser.contentDocument.location), '_blank');
+	 	win.focus();
+	}
   },
 
 }
