6b83b69d9f7fcf24f9bdb79b4df61904f1b96134
James <james@jarofgreen.co.uk>
2014-11-16 15:05:06 +0000

    First commit!

---
+++ chrome/button.js
@@ -0,0 +1,8 @@
+	CustomButton = {
+
+1: function () {
+	var win = window.open("https://www.blocked.org.uk/results?url=" + encodeURIComponent(gBrowser.contentDocument.location), '_blank');
+ 	win.focus();
+  },
+
+}
